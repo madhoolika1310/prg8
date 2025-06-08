5e4d306d0dbb52f673ce2accf950dc26b2d59501
madhoolika1310 <madhoolikatk1215@gmail.com>
2025-06-09 00:38:06 +0530

    first commit

---
+++ prg8/src/main/java/com/madhoolika1310/App.java
@@ -0,0 +1,13 @@
+package com.madhoolika1310;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
